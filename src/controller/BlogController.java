f459ecc2a5a83299c461f5ac0d9cc79e8a6dd3ea
Preethi-kasinathan <61906137+Preethi-kasinathan@users.noreply.github.com>
2020-03-27 17:40:23 +0530

    Add files via upload

---
+++ src/controller/BlogController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import model.Blog;
+import model.User;
+
+@WebServlet(urlPatterns= {"/blog"})
+public class BlogController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public BlogController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
+		rd.forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String blogDetails = request.getParameter("selectedAnswers");
+	String[] blogDetail = blogDetails.split(",");
+	
+	Blog blog = new Blog(blogDetail[0], blogDetail[1], null);
+	User user = new User(blogDetail[0], blogDetail[1], null);
+	
+	blog.setTitle("THE BLOG");
+	blog.setDescription("THIS IS MY SAMPLE BLOG");
+	LocalDateTime dt = LocalDateTime.now();
+	LocalDate d = null;
+	blog.setPostedOn(d);
+	
+		if(blog!=null) {
+			request.setAttribute("blog", blog);
+			request.setAttribute("user",user);
+			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
+			rd.forward(request, response);
+		}
+	
+	}
+
+}
